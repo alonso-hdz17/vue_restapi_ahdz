ed41ea4eb1f3dbe286617d63a5ecd685b78b3f8c
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-08 23:56:19 -0500

    configured button

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,10 +1,30 @@
 <script setup>
-import { ref } from 'vue'
-const title = ref('Hello There ✋')
+import { onMounted } from 'vue'
+import BaseCard from '@/components/BaseCard.vue'
+import useCharacters from '@/composables/useCharacters'
+
+const { character, fetchCharacters, firstLoad } = useCharacters()
+
+onMounted(async () => {
+  if (firstLoad.value) {
+    await fetchCharacters()
+    firstLoad.value = false
+  }
+})
 </script>
 
 <template>
-  <main class="flex min-h-screen items-center justify-center">
-    <h1 class="text-6xl font-thin text-slate-800">{{ title }}</h1>
+  <button
+    title="Load Characters"
+    class="z-90 fixed bottom-5 right-3 flex h-16 w-16 flex-auto items-center justify-center rounded-full bg-fuchsia-400 text-4xl text-white drop-shadow-lg duration-300 hover:scale-110 hover:bg-fuchsia-500 hover:drop-shadow-2xl"
+    @click="fetchCharacters"
+  >
+    🔫
+  </button>
+
+  <main class="min-h-screen bg-gray-900">
+    <div class="container mx-auto grid grid-cols-5 gap-6 py-9">
+      <BaseCard v-for="character in character" :key="character.id" :character="character" />
+    </div>
   </main>
 </template>
