6563ff62b3545222f71fd9c7dbb160b1047be880
alonso-hdz17 <alonsohdz0783@gmail.com>
2022-10-08 23:57:02 -0500

    styled current character

---
+++ src/views/CharacterDetails.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted, onUnmounted } from 'vue'
+import { useRoute } from 'vue-router'
+import useCharacters from '@/composables/useCharacters'
+
+const route = useRoute()
+const { fetchCharacter, currentCharacter } = useCharacters()
+onMounted(async () => {
+  await fetchCharacter(route.params.id)
+})
+onUnmounted(() => {
+  currentCharacter.value = null
+})
+</script>
+
+<template>
+  <main class="min-h-screen bg-gray-900 py-7 text-white">
+    <div v-if="currentCharacter" class="flex flex-col items-center justify-center gap-5">
+      <img :src="currentCharacter.image" :alt="currentCharacter.name" />
+      <h1 class="font-serif text-6xl font-thin text-green-500">I'm {{ currentCharacter.name }}</h1>
+    </div>
+  </main>
+</template>
